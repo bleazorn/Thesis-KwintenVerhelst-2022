19329a4f49930273ca28372b7ced9459d228d12c
bleazorn <only_forgames@hotmail.com>
2022-10-17 19:16:57 +0200

    assign-fvars now assigns some registers first

--- Compiler/Milestone2/assign-fvars.rkt
+++ Compiler/Milestone2/assign-fvars.rkt
@@ -6,12 +6,26 @@
 (module+ test
   (require rackunit))
 
+(define l -1)
 
+(define (resetLocal)
+  (set! l -1))
+
+(define (getLocal)
+  (set! l (add1 l))
+  l)
+
+;use t0, t1, t2, t3 dan pas memory
+(define (freshLocal)
+  (if (< l 3)
+      (string->symbol (format "t~a" (getLocal)))
+      (freshfvar)))
+      
 ;
 ;(assign-locals locals)->assignment
 ;locals: locals
 (define (assign-locals locals)
-  `(assignment ,(map (lambda (l) (list l (freshfvar))) locals)))
+  `(assignment ,(map (lambda (l) (list l (freshLocal))) locals)))
 
 ;Compiles Asm-lang-V2-locals to Asm-lang-V2-assignments, by assigning each abstract location from the locals info field to a fresh frame variable.
 ;(assign-fvars p) → Asm-lang-V2-assignments?
@@ -24,8 +38,12 @@
 
 (module+ test
 ;assign-fvars
+  (define (check-assign t1 t2 text)
+    (resetLocal)
+    (resetfvar)
+    (check-equal? t1 t2 text))
   ;succes
-  (check-equal?
+  (check-assign
    (assign-fvars
     '(module
          ((locals (x.1)))
@@ -34,11 +52,10 @@
          (halt x.1))))
    '(module
         ((locals (x.1))
-         (assignment ((x.1 fv0))))
+         (assignment ((x.1 t0))))
       (begin (set! x.1 0) (halt x.1)))
    "assign-fvars: succes-1: one location")
-  (resetfvar)
-  (check-equal?
+  (check-assign
    (assign-fvars
     '(module
          ((locals (x.1 y.1 w.1)))
@@ -48,7 +65,22 @@
          (set! w.1 (+ w.1 y.1))
          (halt w.1))))
    '(module
-        ((locals (x.1 y.1 w.1)) (assignment ((x.1 fv0) (y.1 fv1) (w.1 fv2))))
+        ((locals (x.1 y.1 w.1)) (assignment ((x.1 t0) (y.1 t1) (w.1 t2))))
       (begin (set! x.1 0) (set! y.1 x.1) (set! w.1 (+ w.1 y.1)) (halt w.1)))
-   "assign-fvars: succes-1: multiple locations")
+   "assign-fvars: succes-1: multiple locations still in register")
+  (check-assign
+   (assign-fvars
+    '(module
+         ((locals (x.1 y.1 w.1 a.1 b.1)))
+       (begin
+         (set! x.1 0)
+         (set! y.1 x.1)
+         (set! w.1 (+ w.1 y.1))
+         (set! a.1 1)
+         (set! b.1 2)
+         (halt w.1))))
+   '(module
+        ((locals (x.1 y.1 w.1 a.1 b.1)) (assignment ((x.1 t0) (y.1 t1) (w.1 t2) (a.1 t3) (b.1 fv0))))
+      (begin (set! x.1 0) (set! y.1 x.1) (set! w.1 (+ w.1 y.1)) (set! a.1 1) (set! b.1 2) (halt w.1)))
+   "assign-fvars: succes-1: multiple locations one in memory")
   )
