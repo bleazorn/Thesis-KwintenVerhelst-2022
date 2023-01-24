851970711950f93a19a6100613a444a3108f9cab
Sander Huyghebaert <sanderhuyghebaert@yahoo.com>
2023-01-24 06:58:51 +0100

    Fixed some remaining tests

--- Compiler/Milestone6s/120-3-assign-registers-full.rkt
+++ Compiler/Milestone6s/120-3-assign-registers-full.rkt
@@ -117,13 +117,14 @@
   (check-equal? (assign-registers-full '(module ((locals (x.1))
                                             (conflicts ((x.1 ())))
                                             (new-frames ())
-                                            (call-undead ()))
+                                            (call-undead ())
+                                            (allocatedFvars ()))
                                      (begin
                                        (set! x.1 42)
                                        (set! x.1 x.1)
                                        (jump L.foo.4))))
                 '(module
-                     ((assignment ((x.1 t0))) (locals (x.1)) (conflicts ((x.1 ()))) (new-frames ()) (call-undead ()))
+                     ((assignment ((x.1 t0))) (locals (x.1)) (conflicts ((x.1 ()))) (new-frames ()) (call-undead ()) (allocatedFvars ()))
                    (begin (set! x.1 42) (set! x.1 x.1) (jump L.foo.4)))
                 "assign-registers: succes-1 one instruction")
   (check-equal? (assign-registers-full '(module ((locals (v.1 w.2 x.3 y.4 z.5 t.6 p.1))
@@ -136,7 +137,8 @@
                                               (z.5 (t.6 p.1 y.4 w.2 x.3))
                                               (t.6 (z.5 p.1 y.4))))
                                              (new-frames ())
-                                             (call-undead ()))
+                                             (call-undead ())
+                                             (allocatedFvars ()))
                                      (begin
                                        (set! v.1 1)
                                        (set! w.2 46)
@@ -167,7 +169,8 @@
                         (z.5 (t.6 p.1 y.4 w.2 x.3))
                         (t.6 (z.5 p.1 y.4))))
                       (new-frames ())
-                      (call-undead ()))
+                      (call-undead ())
+                      (allocatedFvars ()))
                    (begin
                      (set! v.1 1)
                      (set! w.2 46)
